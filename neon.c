fa742148c2a2e606032de4445613107dc8b6ee6b
Bartek Makowski <biuro@mackode.pl>
2024-11-15 18:45:57 +0000

    Add basic func interfaces

--- neon.c
+++ neon.c
@@ -15,10 +15,39 @@ typedef struct {
 } maxret_t;
 
 static inline double getTimeInSec(void) {
+  double dtime = -1.0;
+  struct timeval tv;
 
+  if(gettimeofday(&tv, NULL) == 0) {
+    dtime = ((double)tv.tv_sec) + ((double)tv.tv_usec) * 1e-6;
+  }
+
+  return(dtime);
 }
 
+maxret_t findMax(int N, float *xval) {
+maxret_t finMaxVec(int N, float *xval) {
+
 int main(int argc, char *argv[]) {
+  int n;
+  int N = 1024 * 1024 + 1;
+  size_t msize;
+  maxret_t mret = {-1, 0.0};
+  float *xval = NULL;
+  double time1, duration;
+  double rate, membw;
+
+  const int Nlimit = 256 * 1024 * 1024;
+  
+  for (n = 1; n < argc; n++) {
+    if(strcmp(argv[n], "-h") == 0) {
+      printf("neon [-h] [-n #samples]\n");
+      return(0);
+    } else if(strcmp(argv[n], "-n") == 0) {
+
+    }
+  }
+
   return(0);
 }
 
